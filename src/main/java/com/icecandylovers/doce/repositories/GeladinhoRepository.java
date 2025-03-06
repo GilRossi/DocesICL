f39e127b7ba63f00403174cd901a7824a0d3f93e
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-03-06 20:19:37 -0300

    Adicionado Repository para os gelinhos

--- src/main/java/com/icecandylovers/doce/repositories/GeladinhoRepository.java
+++ src/main/java/com/icecandylovers/doce/repositories/GeladinhoRepository.java
@@ -1,4 +1,9 @@
 package com.icecandylovers.doce.repositories;
 
-public interface GeladinhoRepository {
+import com.icecandylovers.doce.models.Geladinho;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface GeladinhoRepository extends JpaRepository<Geladinho, UUID> {
 }
