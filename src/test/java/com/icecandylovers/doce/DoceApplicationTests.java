9dd56ab8216aee42f01bbc1fa07fae2b73e1d3ae
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-03-06 19:12:41 -0300

    Ice Candy Lovers

---
+++ src/test/java/com/icecandylovers/doce/DoceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icecandylovers.doce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
