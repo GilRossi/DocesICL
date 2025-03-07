ed2c310874bb2ea1dea3e184e9816094dee971d7
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-03-06 23:20:34 -0300

    layout

--- src/main/java/com/icecandylovers/doce/controllers/GelinhoController.java
+++ src/main/java/com/icecandylovers/doce/controllers/GelinhoController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 
@@ -26,13 +27,20 @@ public class GelinhoController {
     @RequestMapping(value = "/cadastrarGelinho", method = RequestMethod.POST)
     public String form(@Valid @ModelAttribute("geladinho") Geladinho geladinho, BindingResult result) {
         if (result.hasErrors()) {
-            return "formGelinho"; // Retorna para o formulário se houver erro
+            // Retorna para o formulário se houver erro
+            return "formGelinho";
         }
-
         // Calcula a validade (fab + 90 dias)
         geladinho.setVal(geladinho.getFab().plusDays(90));
-
         geladinhoRepository.save(geladinho);
         return "redirect:/cadastrarGelinho";
+    }
+
+    @RequestMapping("/gelinhos")
+    public ModelAndView listaGelinhos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Geladinho> gelinhos = geladinhoRepository.findAll();
+        mv.addObject("gelinhos", gelinhos);
+        return mv;
     }
 }
